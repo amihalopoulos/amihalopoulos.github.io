698a6f0639cb431f02969f3f032e177e07dfcf29
Alexander Mihalopoulos <amihalop@gmail.com>
2015-09-08 12:10:20 -0400

    add clock and refresh

--- mines2/application.js
+++ mines2/application.js
@@ -12,8 +12,28 @@ $(document).ready(function() {
         break;
     }
     if (game.checkForWin()){
-      alert("YOU WIN!")
+      alert("YOU WIN! Time: " + $('#clock').text())
     }
+
     $('#container').html(game.render())
   });
+  var sec = 0;
+  function pad ( val ) { return val > 9 ? val : "0" + val; }
+  function reset () {
+    sec = 0
+
+  }
+  setInterval( function(){
+      $("#seconds").html(pad(++sec%60));
+      $("#minutes").html(pad(parseInt(sec/60,10)));
+  }, 1000);
+
+  $('#refresh').click(function(event){
+    console.log(game)
+    game.newGame()
+    $('#container').html(game.render())
+    reset()
+  })
+
 });
+
