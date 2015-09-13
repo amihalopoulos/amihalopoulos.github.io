12afa3cf5e3d5de94a7eb6c69d21ff26b27e4137
Alexander Mihalopoulos <amihalop@gmail.com>
2015-09-13 19:20:18 -0400

    clock with bugs

--- mines2/application.js
+++ mines2/application.js
@@ -1,14 +1,29 @@
 $(document).ready(function() {
-  game = new Game(81);
-  $('#container').html(game.render())
+  $('#beginner').on('click', function(event){
+    game = new Game(9,9);
+    $('#container').html(game.render())
+    clock()
+  })
+  $('#intermediate').on('click', function(event){
+    game = new Game(16,16);
+    $('#container').html(game.render())
+    clock()
+  })
+  $('#expert').on('click', function(event){
+    game = new Game(30,16);
+    $('#container').html(game.render())
+    clock()
+  })
+
 
   $('body').on('mousedown', '.cell', function(event) {
     switch (event.which) {
       case 1:
-        game.click(this.id)
+      // console.log(this.id)
+        game.click(this.id.split(' '))
         break;
       case 3:
-        game.flag(this.id)
+        game.flag(this.id.split(' '))
         break;
     }
     if (game.checkForWin()){
@@ -17,19 +32,9 @@ $(document).ready(function() {
 
     $('#container').html(game.render())
   });
-  var sec = 0;
-  function pad ( val ) { return val > 9 ? val : "0" + val; }
-  function reset () {
-    sec = 0
 
-  }
-  setInterval( function(){
-      $("#seconds").html(pad(++sec%60));
-      $("#minutes").html(pad(parseInt(sec/60,10)));
-  }, 1000);
 
   $('#refresh').click(function(event){
-    console.log(game)
     game.newGame()
     $('#container').html(game.render())
     reset()
@@ -37,3 +42,14 @@ $(document).ready(function() {
 
 });
 
+var clock = function(){
+  var sec = 0;
+  function pad ( val ) { return val > 9 ? val : "0" + val; }
+  function reset () {
+    sec = 0
+  }
+  setInterval( function(){
+      $("#seconds").html(pad(++sec%60));
+      $("#minutes").html(pad(parseInt(sec/60,10)));
+  }, 1000);
+}
